5931a96c8fbf8e406f0985d1c85371d5db5d27e6
Michał Kalinowski <michal@kalinowski.one>
2024-01-08 11:55:55 +0100

    feat: add Client and Session

---
+++ src/connection/client.ts
@@ -0,0 +1,36 @@
+import { type CompactString, type NullableString } from '../protocol/primitives';
+import { type RequestBuilderResponseType, type RequestBuilder } from '../protocol/requests/request-builder';
+import { Session } from './session';
+
+export class Client {
+  private session: Session | null = null;
+
+  constructor(
+    private readonly host: string,
+    private readonly port: number,
+    private readonly clientId: NullableString,
+    private readonly clientSoftwareName: CompactString,
+    private readonly clientSoftwareVersion: CompactString
+  ) {}
+
+  public async send<T extends RequestBuilder<any>>(requestBuilder: T): Promise<RequestBuilderResponseType<T>> {
+    const session = await this.getOrCreateSession();
+
+    return session.send(requestBuilder);
+  }
+
+  private async getOrCreateSession(): Promise<Session> {
+    //todo: consider changing this condition
+    if (this.session === null || !this.session.isHealthy()) {
+      this.session = await Session.create(
+        this.host,
+        this.port,
+        this.clientId,
+        this.clientSoftwareName,
+        this.clientSoftwareVersion
+      );
+    }
+
+    return this.session;
+  }
+}
